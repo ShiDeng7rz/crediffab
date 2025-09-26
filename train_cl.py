f9dffbe802c14195af8efd54ba08cb24de328ee5
liupeishuo <liupeishuo@sensedeal.ai>
2025-09-26 10:33:33 +0800

    modify

--- train_cl.py
+++ train_cl.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     parser.add_argument('config', type=str)
     parser.add_argument('--logdir', type=str, default='./logs')
     parser.add_argument('--debug', action='store_true', default=False)
-    parser.add_argument('--device', type=str, default='cuda:2')
+    parser.add_argument('--device', type=str, default='cuda:0')
     parser.add_argument('--num_workers', type=int, default=8)
     parser.add_argument('--tag', type=str, default='')
     parser.add_argument('--resume', type=str, default=None)
